84c2f217c13890f5344eac4484ab9f9cd9b60a76
Фанта Петро <c.fanta.petro@student.uzhnu.edu.ua>
2023-05-19 07:47:39 +0300

    Dao

--- src/main/java/com/fanta/moneywithsoul/database/DataBaseConfig.java
+++
@@ -1,7 +0,0 @@
-package com.fanta.moneywithsoul.database;
-
-public interface DataBaseConfig {
-    String url = "jdbc:postgresql://containers-us-west-91.railway.app:7090/railway";
-    String user = "postgres";
-    String password = "PkFOJmLy036WZ8hpzqbM";
-}
